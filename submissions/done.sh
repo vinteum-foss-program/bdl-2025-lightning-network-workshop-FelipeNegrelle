6054404b8d1ead05162645478688d5f52314ab24
Felipe Negrelle <83318978+FelipeNegrelle@users.noreply.github.com>
2025-12-23 16:23:01 -0300

    feat: passing the challenge

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,6 +1,5 @@
 # When you finish the workshop:
 # 1. add a link to your replit fork in the next line
-#
-#
+# https://replit.com/@FelipeNegrelle/Programming-Lightning-Intro-to-Payment-Channels-mini?v=1
 # 2. change the FAIL string to PASS.
-echo "FAIL"
+echo "PASS"
